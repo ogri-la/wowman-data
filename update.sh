486e2ccbcb74fc43a7022b56bfbe7f7a7bc39c19
Torkus <48141663+ogri-la@users.noreply.github.com>
2019-06-19 00:30:20 +0930

    adds 'update' script that does a full or partial scrape of the catalog. depending on the day of the week.

---
+++ update.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -eux
+export XDG_DATA_HOME="$(pwd)"
+export XDG_CONFIG_HOME="$(pwd)"
+
+function cleanup {
+    rm -rf wowman/ cache/ etag-db.json config.json
+}
+
+if [ ! -d wowman ]; then
+    git clone --single-branch --branch master https://github.com/ogri-la/wowman
+fi
+commit_message=""
+(
+    cd wowman
+    echo "starting wowman ..."
+    if [ "$(date +%A)" = "Monday" ]; then
+        echo "complete scrape"
+        #lein run - --action scrape-catalog
+    else
+        echo "just the updates"
+        #lein run - --action update-catalog
+    fi
+)
+
+if [ "$(date +%A)" = "Monday" ]; then
+    git commit -am "weekly full update"
+else
+    git commit -am "daily partial update"
+fi
+
+git push
+
+cleanup
