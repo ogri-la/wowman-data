55cdc2115a6af2f4b7b36c00649c121d436ba93e
Torkus <48141663+ogri-la@users.noreply.github.com>
2019-10-03 00:38:24 +0930

    renamed source catalog files to new+consistent format

--- travis-update.sh
+++ travis-update.sh
@@ -4,24 +4,12 @@ export XDG_DATA_HOME="$(pwd)"
 export XDG_CONFIG_HOME="$(pwd)"
 
 if [ ! -d wowman ]; then
-    # TODO: switch back to 'master' branch once bugs ironed out
     git clone --single-branch --branch develop https://github.com/ogri-la/wowman
 fi
+
 (
     cd wowman
-    echo "starting wowman ..."
-    if [ "$(date +%A)" = "Monday" ]; then
-        echo "complete scrape"
-        lein run - --action scrape-catalog
-    else
-        echo "just the updates"
-        lein run - --action update-catalog
-    fi
+    lein run - --action scrape-catalog
 )
 
-# used in .travis-update.sh as the commit message
-if [ "$(date +%A)" = "Monday" ]; then
-    echo "weekly full update" > commit-message.txt
-else
-    echo "daily partial update" > commit-message.txt
-fi
+echo "$(date -I) complete catalog update" > commit-message.txt
